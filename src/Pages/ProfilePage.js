6ab8e2a269fd999bc8f070691ac5236f6b9bf620
hsingwenhsu <hh2916@columbia.edu>
2022-12-03 12:42:08 -0500

    login test

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import { React, useEffect, useState } from "react";
+
+import Button from "@mui/material/Button";
+import { httpCall } from "../utils/httpCall";
+import { useCookies } from "react-cookie";
 
 function ProfilePage () {
-    return <div>Profile Page</div>
+    const [cookies, setCookie] = useCookies(['session']);
+    const handleClickLogin = async () => {
+        console.log('handleClickLogin')
+        const response = await httpCall('http://127.0.0.1:5000/login/google', "GET", null, null);
+        console.log('response', response);
+    }
+    
+    useEffect(() => {
+        console.log('cookie', cookies)
+      }, []);
+
+    return (
+        <div>
+            <Button onClick={handleClickLogin}>Login</Button>
+        </div>
+    )
 }
 
 export default ProfilePage;
