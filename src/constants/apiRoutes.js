5c0ec18ca35a4400c6b2ee017d5e3eb044b16aae
hsingwenhsu <hh2916@columbia.edu>
2022-12-10 01:25:48 -0500

    change get all dog api host

--- src/constants/apiRoutes.js
+++ src/constants/apiRoutes.js
@@ -1,4 +1,4 @@
-const getAllDogs = `${process.env.REACT_APP_DOGINFO_API_HOST}/dogs`;
+const getAllDogs = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT}/dogs`;
 const loginRoute = `${process.env.REACT_APP_USER_API_HOST}/login`;
 const logoutRoute = `${process.env.REACT_APP_USER_API_HOST}/logout`;
 const quizBasePath = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT}/quiz`;
